ca6188fc09a3e7955a9b16a5c99b3c06e0045b05
Mathieu Halle <m@civic-press.org>
2025-11-20 21:54:59 -0500

    style: adapt logo colors to light/dark mode

--- app/components/AppLogo.vue
+++ app/components/AppLogo.vue
@@ -3,9 +3,9 @@
     <img
       src="/logo.svg"
       alt="CivicPress logo"
-      class="h-6 w-6"
+      class="h-6 w-6 dark:invert"
     >
-    <span class="text-base font-semibold tracking-tight">
+    <span class="text-base font-semibold tracking-tight text-gray-900 dark:text-white">
       CivicPress
     </span>
   </div>
